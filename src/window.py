643910d0b2e45c4d2a337b3dac8f6c5120b517d1
Lucas Loura <oliverloura2020@gmail.com>
2025-12-18 19:48:12 -0300

    absolutely necessary refactoring

--- src/window.py
+++ src/window.py
@@ -41,11 +41,11 @@ class DuelpyWindow(Adw.ApplicationWindow):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.create_action('rock', self.on_play, 'rock', ['1'])
-        self.create_action('paper', self.on_play, 'paper', ['2'])
-        self.create_action('scissors', self.on_play, 'scissors', ['3'])
-        self.create_action('lizard', self.on_play, 'lizard', ['4'])
-        self.create_action('spock', self.on_play, 'spock', ['5'])
+        self.create_action('rock', self.on_play, 'Rock', ['1'])
+        self.create_action('paper', self.on_play, 'Paper', ['2'])
+        self.create_action('scissors', self.on_play, 'Scissors', ['3'])
+        self.create_action('lizard', self.on_play, 'Lizard', ['4'])
+        self.create_action('spock', self.on_play, 'Spock', ['5'])
         self.create_action('retry', self.on_retry, shortcuts=['<Ctrl>R'])
 
         self.create_action('show-help-overlay', self.on_show_shortcuts, shortcuts=['<Ctrl>question'])
@@ -63,24 +63,13 @@ class DuelpyWindow(Adw.ApplicationWindow):
             app.set_accels_for_action(f"win.{name}", shortcuts)
 
     def on_play(self, player_choice):
-        choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
         computer_choice = random.choice(choices)
 
         result = self.determine_winner(player_choice, computer_choice)
 
-        #print(f"player_choice: {player_choice}")
-        #print(f"computer_choice: {computer_choice}")
-        #print(f"result: {result}")
-
-        if (player_choice == "spock"):
-            self.img_player_choice.set_from_icon_name("alien")
-        else:
-            self.img_player_choice.set_from_icon_name(player_choice)
-
-        if (computer_choice == "spock"):
-            self.img_computer_choice.set_from_icon_name("alien")
-        else:
-            self.img_computer_choice.set_from_icon_name(computer_choice)
+        self.img_player_choice.set_from_icon_name(player_choice.lower())
+        self.img_computer_choice.set_from_icon_name(computer_choice.lower())
 
         self.lbl_result.set_text(result)
         self.lbl_explanation.set_text(self.get_explanation(player_choice, computer_choice))
@@ -93,11 +82,11 @@ class DuelpyWindow(Adw.ApplicationWindow):
             return _("Draw!")
 
         wins = {
-            'rock': ['scissors', 'lizard'],
-            'paper': ['rock', 'spock'],
-            'scissors': ['paper', 'lizard'],
-            'lizard': ['spock', 'paper'],
-            'spock': ['scissors', 'rock']
+            'Rock': ['Scissors', 'Lizard'],
+            'Paper': ['Rock', 'Spock'],
+            'Scissors': ['Paper', 'Lizard'],
+            'Lizard': ['Spock', 'Paper'],
+            'Spock': ['Scissors', 'Rock']
         }
 
         if computer_choice in wins[player_choice]:
@@ -107,63 +96,50 @@ class DuelpyWindow(Adw.ApplicationWindow):
 
     def get_explanation(self, player_choice, computer_choice):
         if (player_choice == computer_choice):
-            if (player_choice == "rock"):
-                return _("Rock evens out with Rock!")
-            if (player_choice == "paper"):
-                return _("Paper evens out with Paper!")
-            if (player_choice == "scissors"):
-                return _("Scissors evens out with Scissors!")
-            if (player_choice == "lizard"):
-                return _("Lizard evens out with Lizard!")
-            if (player_choice == "spock"):
-                return _("Spock evens out with Spock!")
+            return player_choice + " " + _("evens out with") + " " + computer_choice + "!"
 
         match(player_choice):
-            case "rock":
-                if (computer_choice == "scissors"):
-                    return _("Rock smashes Scissors!")
-                if (computer_choice == "lizard"):
-                    return _("Rock smashes Lizard!")
-                if (computer_choice == "paper"):
-                    return _("Rock gets covered by Paper!")
-                if (computer_choice == "spock"):
-                    return _("Rock gets vaporized by Spock!")
-            case "paper":
-                if (computer_choice == "rock"):
-                    return _("Paper covers Rock!")
-                if (computer_choice == "spock"):
-                    return _("Paper disproves Spock!")
-                if (computer_choice == "scissors"):
-                    return _("Paper gets cut by Scissors!")
-                if (computer_choice == "lizard"):
-                    return _("Paper gets eaten by Lizard!")
-            case "scissors":
-                if (computer_choice == "paper"):
-                    return _("Scissors cuts Paper!")
-                if (computer_choice == "lizard"):
-                    return _("Scissors decapites Lizard!")
-                if (computer_choice == "rock"):
-                    return _("Scissors gets smashed by Rock!")
-                if (computer_choice == "spock"):
-                    return _("Scissors gets smashed by Spock!")
-            case "lizard":
-                if (computer_choice == "paper"):
-                    return _("Lizard eats Paper!")
-                if (computer_choice == "spock"):
-                    return _("Lizard poisons Spock!")
-                if (computer_choice == "scissors"):
-                    return _("Lizard gets decapited by Scissors!")
-                if (computer_choice == "rock"):
-                    return _("Lizard gets smashed by Rock!")
-            case "spock":
-                if (computer_choice == "rock"):
-                    return _("Spock vaporizes Rock!")
-                if (computer_choice == "scissors"):
-                    return _("Spock smashes scissors!")
-                if (computer_choice == "paper"):
-                    return _("Spock gets disproven by Paper!")
-                if (computer_choice == "lizard"):
-                    return _("Spock gets poisoned by Lizard!")
+            case "Rock":
+                if (computer_choice == "Scissors" or computer_choice == "Lizard"):
+                    return player_choice + " " + _("smashes") + " " + computer_choice + "!"
+                if (computer_choice == "Paper"):
+                    return player_choice + " " + _("gets covered by") + " " + computer_choice + "!"
+                if (computer_choice == "Spock"):
+                    return player_choice + " " + _("gets vaporized by") + " " + computer_choice + "!"
+            case "Paper":
+                if (computer_choice == "Rock"):
+                    return player_choice + " " + _("covers") + " " + computer_choice + "!"
+                if (computer_choice == "Spock"):
+                    return player_choice + " " + _("disproves") + " " + computer_choice + "!"
+                if (computer_choice == "Scissors"):
+                    return player_choice + " " + _("gets cut by") + " " + computer_choice + "!"
+                if (computer_choice == "Lizard"):
+                    return player_choice + " " + _("gets eaten by") + " " + computer_choice + "!"
+            case "Scissors":
+                if (computer_choice == "Paper"):
+                    return player_choice + " " + _("cuts") + " " + computer_choice + "!"
+                if (computer_choice == "Lizard"):
+                    return player_choice + " " + _("decapitates") + " " + computer_choice + "!"
+                if (computer_choice == "Rock" or computer_choice == "Spock"):
+                    return player_choice + " " + _("gets smashed by") + " " + computer_choice + "!"
+            case "Lizard":
+                if (computer_choice == "Paper"):
+                    return player_choice + " " + _("eats") + " " + computer_choice + "!"
+                if (computer_choice == "Spock"):
+                    return player_choice + " " + _("poisons") + " " + computer_choice + "!"
+                if (computer_choice == "Scissors"):
+                    return player_choice + " " + _("gets decapitated by") + " " + computer_choice + "!"
+                if (computer_choice == "Rock"):
+                    return player_choice + " " + _("gets smashed by") + " " + computer_choice + "!"
+            case "Spock":
+                if (computer_choice == "Rock"):
+                    return player_choice + " " + _("vaporizes") + " " + computer_choice + "!"
+                if (computer_choice == "Scissors"):
+                    return player_choice + " " + _("smashes") + " " + computer_choice + "!"
+                if (computer_choice == "Paper"):
+                    return player_choice + " " + _("gets disproven by") + " " + computer_choice + "!"
+                if (computer_choice == "Lizard"):
+                    return player_choice + " " + _("gets poisoned by") + " " + computer_choice + "!"
 
     def on_retry(self):
         if self.navigation_view.get_visible_page_tag() == "results":
