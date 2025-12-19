b6cd0049b37794c8ccb7d0e130962a80fd627de3
Lucas Loura <oliverloura2020@gmail.com>
2025-12-19 19:20:50 -0300

    implemented game mode system and refactored blueprint ui
    duel! has been updated into a multi-mode game, adding a 'classic' mode alongside rpsls
    
    ui changes:
        added 'mode_selector' and 'adw.viewstack'
        updated shortcuts dialog
    
    logic Changes:
        created 'constants.py'
        implemented GSettings for persistence
        refactored game logic for scalability

--- src/window.py
+++ src/window.py
@@ -1,161 +1,166 @@
 # window.py
-#
 # Copyright 2025 Lucas Loura
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw
-from gi.repository import Gtk
-from gi.repository import Gio
+from gi.repository import Adw, Gtk, Gio, GLib
 import random
 import gettext
 import os
 
+# import our constants
+from .constants import GAME_RULES, ALL_POSSIBLE_MOVES
+
 @Gtk.Template(resource_path='/io/github/lloura/DuelPy/window.ui')
 class DuelpyWindow(Adw.ApplicationWindow):
-    __gtype_name__ = 'DuelpyWindow'
+    __gtype_name__ = "DuelpyWindow"
 
+    # localization setup
     localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
-
     gettext.bindtextdomain('io.github.lloura.DuelPy', localedir)
     gettext.textdomain('io.github.lloura.DuelPy')
-    _ = gettext.gettext
 
+    # we use staticmethod to prevent Python from passing 'self' to gettext
+    _ = staticmethod(gettext.gettext)
+
+    # template children
     navigation_view = Gtk.Template.Child()
+    mode_stack = Gtk.Template.Child()
     img_player_choice = Gtk.Template.Child()
     img_computer_choice = Gtk.Template.Child()
     lbl_result = Gtk.Template.Child()
     lbl_explanation = Gtk.Template.Child()
-
+    mode_label = Gtk.Template.Child()
     shortcuts_dialog = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.create_action('rock', self.on_play, 'rock', ['1','KP_1'])
-        self.create_action('paper', self.on_play, 'paper', ['2','KP_2'])
-        self.create_action('scissors', self.on_play, 'scissors', ['3','KP_3'])
-        self.create_action('lizard', self.on_play, 'lizard', ['4','KP_4'])
-        self.create_action('spock', self.on_play, 'spock', ['5','KP_5'])
-        self.create_action('retry', self.on_retry, shortcuts=['<Ctrl>R'])
+        self.settings = Gio.Settings.new("io.github.lloura.DuelPy")
+        self.create_game_actions()
 
-        self.create_action('show-help-overlay', self.on_show_shortcuts, shortcuts=['<Ctrl>question'])
+        # monitor mode changes in the stack
+        self.mode_stack.connect("notify::visible-child-name", self.on_mode_changed)
+
+        # restore saved mode and apply initial action states
+        saved_mode = self.settings.get_string("game-mode")
+        if saved_mode in GAME_RULES:
+            self.mode_stack.set_visible_child_name(saved_mode)
+            self.sync_mode_ui(saved_mode)
+
+    def create_game_actions(self):
+        """Initialize all window actions and shortcuts"""
+        # Gameplay actions
+        self.create_action("rock", self.on_play, "rock", ["1", "KP_1"])
+        self.create_action("paper", self.on_play, "paper", ["2", "KP_2"])
+        self.create_action("scissors", self.on_play, "scissors", ["3", "KP_3"])
+        self.create_action("lizard", self.on_play, "lizard", ["4", "KP_4"])
+        self.create_action("spock", self.on_play, "spock", ["5", "KP_5"])
+
+        # system actions
+        self.create_action("retry", self.on_retry, shortcuts=["<Ctrl>R"])
+        self.create_action("show-help-overlay", self.on_show_shortcuts, shortcuts=["<Ctrl>question"])
+
+        # mode change action (Stateful)
+        mode_val = self.settings.get_string("game-mode") or "rpsls"
+        mode_action = Gio.SimpleAction.new_stateful(
+            "change-mode",
+            GLib.VariantType.new('s'),
+            GLib.Variant('s', mode_val)
+        )
+        mode_action.connect("activate", self.on_change_mode_activated)
+        self.add_action(mode_action)
+
+        # global accels for mode switching
+        app = self.get_application()
+        app.set_accels_for_action("win.change-mode('rpsls')", ["<Ctrl><Shift>R"])
+        app.set_accels_for_action("win.change-mode('classic')", ["<Ctrl><Shift>C"])
 
     def create_action(self, name, callback, parameter=None, shortcuts=None):
+        """Helper to create SimpleActions quickly"""
         action = Gio.SimpleAction.new(name, None)
-        action.connect(
-            "activate",
-            lambda action, _: callback(parameter) if parameter else callback()
-        )
+        action.connect("activate", lambda a, v: callback(parameter) if parameter else callback())
         self.add_action(action)
-
         if shortcuts:
-            app = self.get_application()
-            app.set_accels_for_action(f"win.{name}", shortcuts)
+            self.get_application().set_accels_for_action(f"win.{name}", shortcuts)
+
+    # --- sync logic ---
+
+    def on_mode_changed(self, stack, param):
+        """Ensures the app behaves correctly when switching modes"""
+        mode = stack.get_visible_child_name()
+        self.settings.set_string("game-mode", mode)
+        self.sync_mode_ui(mode)
+
+        if self.navigation_view.get_visible_page_tag() == "results":
+            self.navigation_view.pop()
+
+    def sync_mode_ui(self, mode):
+        """Updates icons, labels and enables/disables shortcuts"""
+        rules = GAME_RULES.get(mode)
+        if not rules: return
+
+        self.mode_label.set_label(rules["name"])
+        self.mode_label.set_icon_name(rules["icon"])
+
+        # enable only the keys allowed in the current mode
+        for move in ALL_POSSIBLE_MOVES:
+            action = self.lookup_action(move)
+            if action:
+                action.set_enabled(move in rules["choices"])
+
+    # --- game logic ---
 
     def on_play(self, player_choice):
-        choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        mode = self.mode_stack.get_visible_child_name()
+        choices = GAME_RULES[mode]["choices"]
         computer_choice = random.choice(choices)
 
-        result = self.determine_winner(player_choice, computer_choice)
+        result_text = self.determine_winner(mode, player_choice, computer_choice)
 
-        self.img_player_choice.set_from_icon_name(player_choice.lower())
-        self.img_computer_choice.set_from_icon_name(computer_choice.lower())
-
-        self.lbl_result.set_text(result)
+        self.img_player_choice.set_from_icon_name(player_choice)
+        self.img_computer_choice.set_from_icon_name(computer_choice)
+        self.lbl_result.set_text(result_text)
         self.lbl_explanation.set_text(self.get_explanation(player_choice, computer_choice))
 
         if self.navigation_view.get_visible_page_tag() != "results":
             self.navigation_view.push_by_tag("results")
 
-    def determine_winner(self, player_choice, computer_choice):
-        if player_choice == computer_choice:
-            return _("Draw!")
+    def determine_winner(self, mode, player, computer):
+        if player == computer:
+            return self._("Draw!")
 
-        wins = {
-            'rock': ['scissors', 'lizard'],
-            'paper': ['rock', 'spock'],
-            'scissors': ['paper', 'lizard'],
-            'lizard': ['spock', 'paper'],
-            'spock': ['scissors', 'rock']
+        if computer in GAME_RULES[mode]["wins"].get(player, []):
+            return self._("You Won!")
+        return self._("You Lost!")
+
+    def get_explanation(self, player, computer):
+        """Returns the localized string explaining why someone won"""
+        explanations = {
+            ("rock", "scissors"): self._("Rock smashes Scissors!"),
+            ("rock", "lizard"): self._("Rock smashes Lizard!"),
+            ("paper", "rock"): self._("Paper covers Rock!"),
+            ("paper", "spock"): self._("Paper disproves Spock!"),
+            ("scissors", "paper"): self._("Scissors cuts Paper!"),
+            ("scissors", "lizard"): self._("Scissors decapitates Lizard!"),
+            ("lizard", "paper"): self._("Lizard eats Paper!"),
+            ("lizard", "spock"): self._("Lizard poisons Spock!"),
+            ("spock", "rock"): self._("Spock vaporizes Rock!"),
+            ("spock", "scissors"): self._("Spock smashes Scissors!"),
         }
 
-        if computer_choice in wins[player_choice]:
-            return _("You Won!")
-        else:
-            return _("You Lost!")
+        # check both directions (player wins or computer wins)
+        if player == computer:
+            return self._("It's a tie!")
 
-    def get_explanation(self, player_choice, computer_choice):
-        match(player_choice, computer_choice):
-            case ("rock", "rock"):
-                return _("Rock evens out with Rock!")
-            case ("rock", "paper"):
-                return _("Rock gets covered by Paper!")
-            case ("rock", "scissors"):
-                return _("Rock smashes Scissors!")
-            case ("rock", "lizard"):
-                return _("Rock smashes Lizard!")
-            case ("rock", "spock"):
-                return _("Rock gets vaporized by Spock!")
+        return explanations.get((player, computer),
+               explanations.get((computer, player), ""))
 
-            case ("paper", "rock"):
-                return _("Paper covers Rock!")
-            case ("paper", "paper"):
-                return _("Paper evens out with Paper!")
-            case ("paper", "scissors"):
-                return _("Paper gets cut by Scissors!")
-            case ("paper", "lizard"):
-                return _("Paper gets eaten by Lizard!")
-            case ("paper", "spock"):
-                return _("Paper disproves Spock!")
+    # --- ui callbacks ---
 
-            case ("scissors","rock"):
-                return _("Scissors gets smashed by Rock!")
-            case ("scissors","paper"):
-                return _("Scissors cuts Paper!")
-            case ("scissors","scissors"):
-                return _("Scissors evens out with Scissors!")
-            case ("scissors","lizard"):
-                return _("Scissors decapitates Lizard!")
-            case ("scissors","spock"):
-                return _("Scissors gets smashed by Spock!")
-
-            case ("lizard","rock"):
-                return _("Lizard gets smashed by Rock!")
-            case ("lizard","paper"):
-                return _("Lizard eats Paper!")
-            case ("lizard","scissors"):
-                return _("Lizard gets decapitated by Scissors!")
-            case ("lizard","lizard"):
-                return _("Lizard evens out with Lizard!")
-            case ("lizard","spock"):
-                return _("Lizard poisons Spock!")
-
-            case ("spock", "rock"):
-                return _("Spock vaporizes Rock!")
-            case ("spock", "paper"):
-                return _("Spock gets disproven by Paper!")
-            case ("spock", "scissors"):
-                return _("Spock smashes Scissors!")
-            case ("spock", "lizard"):
-                return _("Spock gets poisoned by Lizard!")
-            case ("spock", "spock"):
-                return _("Spock evens out with Spock!")
-
-        return ""
+    def on_change_mode_activated(self, action, parameter):
+        mode = parameter.get_string()
+        action.set_state(parameter)
+        self.mode_stack.set_visible_child_name(mode)
 
     def on_retry(self):
         if self.navigation_view.get_visible_page_tag() == "results":
