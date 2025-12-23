ecffec7bd5439ae504405f96a5737e328b442c23
Lucas Loura <oliverloura2020@gmail.com>
2025-12-22 22:29:58 -0300

    readded mode swtich shortcuts to the shortcuts menu and also added license text to recently created .py files

--- src/ui/widgets.py
+++ src/ui/widgets.py
@@ -1,3 +1,22 @@
+# widgets.py
+#
+# Copyright 2025 Lucas Loura
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from gi.repository import Gtk, Adw
 import gettext
 
@@ -5,15 +24,34 @@ _ = gettext.gettext
 
 def create_shortcuts(title, moves):
     dialog = Adw.ShortcutsDialog(title=title)
-    section = Adw.ShortcutsSection(title=_("Gameplay"))
 
+    gameplay_section = Adw.ShortcutsSection(title=_("Gameplay"))
     for label, accel, action in moves:
         item = Adw.ShortcutsItem(title=label, accelerator=accel, action_name=action)
-        section.add(item)
+        gameplay_section.add(item)
 
-    retry_item = Adw.ShortcutsItem(title=_("Try Again"), accelerator="<Ctrl>R", action_name="win.retry")
-    section.add(retry_item)
-    dialog.add(section)
+    gameplay_section.add(Adw.ShortcutsItem(
+        title=_("Try Again"),
+        accelerator="<Ctrl>R",
+        action_name="win.retry"
+    ))
+    dialog.add(gameplay_section)
+
+    mode_section = Adw.ShortcutsSection(title=_("Game Mode"))
+
+    mode_section.add(Adw.ShortcutsItem(
+        title=_("Switch to Classic Mode"),
+        accelerator="<Ctrl><Shift>C",
+        action_name="win.change-mode('classic')"
+    ))
+
+    mode_section.add(Adw.ShortcutsItem(
+        title=_("Switch to RPSLS Mode"),
+        accelerator="<Ctrl><Shift>R",
+        action_name="win.change-mode('rpsls')"
+    ))
+
+    dialog.add(mode_section)
     return dialog
 
 @Gtk.Template(resource_path='/io/github/lloura/DuelPy/ui/rpsls_view.ui')
